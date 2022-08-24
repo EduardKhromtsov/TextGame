0249279d96ca24bd9940bc159ce0d244e14f4ac3
Леонид Власов <khromtsov03@mail.ru>
2022-08-24 13:52:44 +0300

    В класс Creature добавлено три public-метода: уменьшение здоровья, проверка на смерть и добавление золота.

--- TextGame.cpp
+++ TextGame.cpp
@@ -1,10 +1,16 @@
 ﻿
 #include <iostream>
 
+#include "Creature.h";
+
 
 int main()
 {
-    setlocale(LC_ALL, "RU");
+    Creature o("orc", 'o', 4, 2, 10);
+
+    o.AddGold(5);
+    o.ReduceHealth(1);
+    std::cout << "The " << o.GetName() << " has " << o.GetHealth() << " health and " << o.GetGold() << " gold.\n";
 }
 
 
